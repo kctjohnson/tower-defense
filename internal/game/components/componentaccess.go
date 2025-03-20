403884c75e972364aed1592a279468970ff04599
Killean Johnson <killean.johnson@viewrail.com>
2025-03-20 09:45:11 -0400

    Set up beginning components

--- internal/game/components/componentaccess.go
+++ internal/game/components/componentaccess.go
@@ -10,10 +10,52 @@ func NewComponentAccess(world *ecs.World) *ComponentAccess {
 	return &ComponentAccess{world: world}
 }
 
-func (c *ComponentAccess) GetSampleComponent(entity ecs.Entity) (*SampleComponent, bool) {
-	component, found := c.world.ComponentManager.GetComponent(entity, Sample)
+func GetComponentT[T ecs.ComponentInterface](
+	world *ecs.World,
+	entity ecs.Entity,
+	componentType ecs.ComponentType,
+) (T, bool) {
+	component, found := world.ComponentManager.GetComponent(entity, componentType)
 	if !found {
-		return nil, false
+		var zero T
+		return zero, false
 	}
-	return component.(*SampleComponent), true
+
+	return component.(T), true
+}
+
+func (c *ComponentAccess) GetPositionComponent(entity ecs.Entity) (*PositionComponent, bool) {
+	return GetComponentT[*PositionComponent](c.world, entity, Position)
+}
+
+func (c *ComponentAccess) GetHealthComponent(entity ecs.Entity) (*HealthComponent, bool) {
+	return GetComponentT[*HealthComponent](c.world, entity, Health)
+}
+
+func (c *ComponentAccess) GetVelocityComponent(entity ecs.Entity) (*VelocityComponent, bool) {
+	return GetComponentT[*VelocityComponent](c.world, entity, Velocity)
+}
+
+func (c *ComponentAccess) GetEnemyComponent(entity ecs.Entity) (*EnemyComponent, bool) {
+	return GetComponentT[*EnemyComponent](c.world, entity, Enemy)
+}
+
+func (c *ComponentAccess) GetTowerComponent(entity ecs.Entity) (*TowerComponent, bool) {
+	return GetComponentT[*TowerComponent](c.world, entity, Tower)
+}
+
+func (c *ComponentAccess) GetProjectileComponent(entity ecs.Entity) (*ProjectileComponent, bool) {
+	return GetComponentT[*ProjectileComponent](c.world, entity, Projectile)
+}
+
+func (c *ComponentAccess) GetPathComponent(entity ecs.Entity) (*PathComponent, bool) {
+	return GetComponentT[*PathComponent](c.world, entity, Path)
+}
+
+func (c *ComponentAccess) GetWalletComponent(entity ecs.Entity) (*WalletComponent, bool) {
+	return GetComponentT[*WalletComponent](c.world, entity, Wallet)
+}
+
+func (c *ComponentAccess) GetRenderableComponent(entity ecs.Entity) (*RenderableComponent, bool) {
+	return GetComponentT[*RenderableComponent](c.world, entity, Renderable)
 }
--- internal/game/components/components.go
+++ internal/game/components/components.go
@@ -3,18 +3,108 @@ package components
 import "ecstemplate/pkg/ecs"
 
 const (
-	Sample ecs.ComponentType = "sample"
+	Position   ecs.ComponentType = "position"
+	Health     ecs.ComponentType = "health"
+	Velocity   ecs.ComponentType = "velocity"
+	Enemy      ecs.ComponentType = "enemy"
+	Tower      ecs.ComponentType = "tower"
+	Projectile ecs.ComponentType = "projectile"
+	Path       ecs.ComponentType = "path"
+	Wallet     ecs.ComponentType = "wallet"
+	Renderable ecs.ComponentType = "renderable"
 )
 
-type SampleComponent struct {
+type PositionComponent struct {
 	ecs.Component
-	Data string
+	X, Y float64
 }
 
-func (c SampleComponent) GetType() ecs.ComponentType {
-	return Sample
+func (c PositionComponent) GetType() ecs.ComponentType {
+	return Position
+}
+
+type HealthComponent struct {
+	ecs.Component
+	Current, Max float64
+}
+
+func (c HealthComponent) GetType() ecs.ComponentType {
+	return Health
+}
+
+type VelocityComponent struct {
+	ecs.Component
+	X, Y float64
+}
+
+func (c VelocityComponent) GetType() ecs.ComponentType {
+	return Velocity
+}
+
+type EnemyComponent struct {
+	ecs.Component
+	Type   string
+	Speed  float64
+	Reward float64
+}
+
+func (c EnemyComponent) GetType() ecs.ComponentType {
+	return Enemy
+}
+
+type TowerComponent struct {
+	ecs.Component
+	Damage, Range, Cooldown, LastFired float64
+}
+
+func (c TowerComponent) GetType() ecs.ComponentType {
+	return Tower
+}
+
+type ProjectileComponent struct {
+	ecs.Component
+	Damage, Speed, Reward float64
+}
+
+func (c ProjectileComponent) GetType() ecs.ComponentType {
+	return Projectile
+}
+
+type PathComponent struct {
+	ecs.Component
+	Waypoints []PositionComponent
+}
+
+func (c PathComponent) GetType() ecs.ComponentType {
+	return Path
+}
+
+type WalletComponent struct {
+	ecs.Component
+	Money float64
+}
+
+func (c WalletComponent) GetType() ecs.ComponentType {
+	return Wallet
+}
+
+type RenderableComponent struct {
+	ecs.Component
+	Symbol string
+}
+
+func (c RenderableComponent) GetType() ecs.ComponentType {
+	return Renderable
 }
 
 var ComponentTypes = []ecs.ComponentType{
-	Sample,
+	Position,
+	Health,
+	Velocity,
+	Enemy,
+	Tower,
+	Projectile,
+	Path,
+	Wallet,
+	Renderable,
 }
