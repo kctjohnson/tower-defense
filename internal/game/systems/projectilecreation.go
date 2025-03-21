3d81c8ddc3a7734647e433dfe99b48b04b7f5f24
Killean Johnson <killean.johnson@viewrail.com>
2025-03-21 18:15:23 -0400

    Projectile creation system

--- internal/game/systems/projectilecreation.go
+++ internal/game/systems/projectilecreation.go
@@ -1,1 +1,71 @@
 package systems
+
+import (
+	"math"
+
+	"ecstemplate/internal/game/components"
+	"ecstemplate/pkg/ecs"
+)
+
+// ProjectileCreationSystem handles the shootintents and creates the projectiles accordingly
+type ProjectileCreationSystem struct {
+	ComponentAccess *components.ComponentAccess
+}
+
+func (s *ProjectileCreationSystem) Update(world *ecs.World, deltaTime float64) {
+	// Get all entities with a shoot intent (should only be towers)
+	shootIntentEnts := world.ComponentManager.GetAllEntitiesWithComponent(components.ShootIntent)
+
+	// Loop through all shoot intents
+	for _, shootIntentEnt := range shootIntentEnts {
+		shootIntent, _ := s.ComponentAccess.GetShootIntentComponent(shootIntentEnt)
+
+		// Get the shooter's position
+		shooterPos, _ := s.ComponentAccess.GetPositionComponent(shootIntent.Shooter)
+
+		// Get the target's position
+		targetPos, _ := s.ComponentAccess.GetPositionComponent(shootIntent.Target)
+
+		// Get the angle between the two
+		angle := calcAngleBetweenPoints(*shooterPos, *targetPos)
+
+		// Create the projectile entity with the velocity vector
+		baseProjectileSpeed := 2.0 // This should be a constant
+		projectileEnt := world.EntityManager.CreateEntity()
+		world.ComponentManager.AddComponent(
+			projectileEnt,
+			components.Projectile,
+			&components.ProjectileComponent{
+				Damage:       1,
+				Speed:        baseProjectileSpeed,
+				TargetEntity: shootIntent.Target,
+			},
+		)
+		world.ComponentManager.AddComponent(
+			projectileEnt,
+			components.Position,
+			&components.PositionComponent{
+				X: shooterPos.X,
+				Y: shooterPos.Y,
+			},
+		)
+		world.ComponentManager.AddComponent(
+			projectileEnt,
+			components.Velocity,
+			&components.VelocityComponent{
+				X: math.Cos(angle) * baseProjectileSpeed,
+				Y: math.Sin(angle) * baseProjectileSpeed,
+			},
+		)
+		world.ComponentManager.AddComponent(
+			projectileEnt,
+			components.Renderable,
+			&components.RenderableComponent{
+				Symbol: "o",
+			},
+		)
+
+		// Remove the shoot intent component from the tower
+		world.ComponentManager.RemoveComponent(shootIntentEnt, components.ShootIntent)
+	}
+}
